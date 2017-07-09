4a184c189fafc8c0e7d5532b0ed78651780958f6
Carlo Fontanos <carl.fontanos@gmail.com>
2017-07-09 15:21:59 +0800

    Integrated redux provider.

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import reducers from './src/reducers';
+import MainContainer from './src/containers/MainContainer';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<Provider store={createStore(reducers)}>
+				<MainContainer />
+			</Provider>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
