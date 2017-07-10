a14b931a15fb67a7c2bcdf28fad2eaf42d112562
Carlo Fontanos <carl.fontanos@gmail.com>
2017-07-10 17:48:43 +0800

    Added logic for login and logout with redux store.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {}
+	}
+	
 	render() {
+		let isLoggedIn = this.props.user.isLoggedIn;
 		return (
 			<View style={styles.container}>
 				<View style={styles.navBar}>
-					<Text style={styles.navBarButton}>Back</Text>
-					<Text style={styles.navBarHeader}>Awesome App</Text>
-					<Text style={styles.navBarButton}>More</Text>
+					{!isLoggedIn && 
+						<Text style={styles.navBarHeader}>Please Login</Text>
+					}
+					{isLoggedIn && 
+						<Text style={styles.navBarHeader}>Welcome {this.props.user.name}</Text>
+					}
 				</View>
 				<View style={styles.content}>
-					<Text style={styles.text}>
-						Welcome to Awesome App!
-					</Text>
+					{!isLoggedIn && 
+						<View>
+							<Text style={styles.label}>Username</Text>
+							<TextInput
+								style={styles.textinput}
+								onChangeText={(text) => this.setState({username:text})}
+							/>
+							<Text style={styles.label}>Password</Text>
+							<TextInput
+								style={styles.textinput}
+								onChangeText={(text) => this.setState({password:text})}
+								secureTextEntry
+							/>
+							<Button title="Login" onPress={() => {
+								if(this.state.username && this.state.password){
+									this.props.log_in({
+										authToken: 'fgh8F8ffFj43gghe3FJFFJE4ff9fd9',
+										id: 101,
+										name: this.state.username
+									});
+								}
+							}}/>
+						</View>
+					}
+					{isLoggedIn && 
+						<View>
+							<Button title="Logout" onPress={() => this.props.log_out()}/>
+						</View>
+					}
 				</View>
 			</View>
 		);
+	}
+	
+	submit(){
+		
 	}
 }
 
@@ -24,18 +63,14 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		paddingTop: StatusBar.currentHeight
+		paddingTop: StatusBar.currentHeight,
 	},
 	navBar: {
 		flexDirection: 'row',
 		paddingTop: 25,
+		marginBottom: 20,
 		height: 64,
 		backgroundColor: '#1EAAF1',
-	},
-	navBarButton: {
-		color: '#FFFFFF',
-		textAlign:'center',
-		width: 64
 	},
 	navBarHeader: {
 		flex: 1,
@@ -45,11 +80,18 @@ const styles = StyleSheet.create({
 	},
 	content: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#374046'
+		paddingLeft: 15,
+		paddingRight: 15
 	},
-	text: {
-		color: '#EEEEEE'
+	label: {
+		color: '#333',
+		fontSize: 16,
+		marginTop: 15
 	},
+	textinput: {
+		alignSelf: 'stretch',
+		height: 40, 
+		borderColor: 'white',
+		fontSize: 16,
+	}
 });
